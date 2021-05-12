9949538f52cf11c9248c19b106b9ab9a2610d790
serenahuang1995 <small08261995@gmail.com>
2021-05-12 15:55:23 +0800

    First commit

---
+++ Chores/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Chores
+//
+//  Created by 黃瀞萱 on 2021/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    print("888888888888888888888888888888888888888888")
+  }
+}
