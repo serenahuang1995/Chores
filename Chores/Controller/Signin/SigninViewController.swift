08d2a09f43502ddb70bd48d70dbc51c40f3e8f08
serenahuang1995 <small08261995@gmail.com>
2021-05-31 15:54:12 +0800

    Firebase Auth

--- Chores/Controller/Signin/SigninViewController.swift
+++ Chores/Controller/Signin/SigninViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 import AuthenticationServices
 import FirebaseAuth
 import Lottie
+import CryptoKit
 
 class SigninViewController: UIViewController {
 
     @IBOutlet weak var privacyButton: UIButton!
     
     @IBOutlet weak var signinView: AnimationView!
+    
+    fileprivate var currentNonce: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +26,7 @@ class SigninViewController: UIViewController {
         
         setUpLottie()
     }
-    
-    
+
     @IBAction func skip(_ sender: Any) {
         
         performSegue(withIdentifier: Segue.initial, sender: nil)
@@ -52,7 +54,8 @@ class SigninViewController: UIViewController {
         ])
 
     }
-    
+
+    @available(iOS 13, *)
     @objc func clickAppleSigninButton() {
         
         let provider = ASAuthorizationAppleIDProvider()
@@ -60,6 +63,12 @@ class SigninViewController: UIViewController {
         let request = provider.createRequest()
         
         request.requestedScopes = [.email, .fullName]
+        
+        let nonce = randomNonceString()
+        
+        currentNonce = nonce
+        
+        request.nonce = sha256(nonce)
         
         let controller = ASAuthorizationController(authorizationRequests: [request])
         
@@ -80,19 +89,124 @@ class SigninViewController: UIViewController {
         
         signinView.loopMode = .loop
     }
+    
+    func fetchUser() {
+        
+        UserProvider.shared.fetchUser(userId: <#T##String#>) { result in
+            
+            
+            
+        }
+        
+        
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        
+      precondition(length > 0)
+      let charset: Array<Character> =
+          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+    
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
 
 }
 
+@available(iOS 13.0, *)
 extension SigninViewController: ASAuthorizationControllerDelegate {
     
     func authorizationController(controller: ASAuthorizationController,
                                  didCompleteWithAuthorization authorization: ASAuthorization) {
         
-        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
+        //        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
         // upload credential to api
         
-        print(credential)
+        //        print(credential)
+        
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            
+            guard let nonce = currentNonce else {
+                
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                
+                if let error = error {
+
+                    print(error.localizedDescription)
+                    
+                    return
+                    
+                } else {
+                    
+                    print(authResult!.user)
+                    
+                    guard let user = authResult?.user else { return }
+                    
+                    print(user.email as Any)
+                    
+                    print(user.displayName as Any)
+                    
+                    guard let uid = Auth.auth().currentUser?.uid else { return }
+                    
+                    print(uid)
+                    
+                    let userDefault = UserDefaults()
+                        
+                    userDefault.setValue(uid, forKey: "FirebaseUid")
+                }
+            }
+        }
     }
+    
     func authorizationController(controller: ASAuthorizationController,
                                  didCompleteWithError error: Error) {
         // Show error
