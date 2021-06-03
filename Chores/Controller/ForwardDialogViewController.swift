6647bb90d8d22ffa494984ff7428dbebba063322
serenahuang1995 <small08261995@gmail.com>
2021-06-04 01:31:50 +0800

    UI draft, show the view if user recive forward dialog

---
+++ Chores/Controller/ForwardDialogViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ForwardDialogViewController.swift
+//  Chores
+//
+//  Created by 黃瀞萱 on 2021/6/4.
+//
+
+import UIKit
+
+class ForwardDialogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
