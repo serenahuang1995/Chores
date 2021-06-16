3c845119fbbbccc74e855b4ab38f54494c1da716
serenahuang1995 <small08261995@gmail.com>
2021-06-16 23:38:56 +0800

    Refactor RecordsTableViewCell variable name

--- Chores/View/Profile/RecordsTableViewCell.swift
+++ Chores/View/Profile/RecordsTableViewCell.swift
@@ -9,31 +9,36 @@ import UIKit
 
 class RecordsTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var recordView: CardView!
+    @IBOutlet weak var cardView: CardView! {
+        
+        didSet {
+            
+            cardView.backgroundColor = .beigeFFF1E6
+        }
+    }
     
     @IBOutlet weak var choreImage: UIImageView!
     
-    @IBOutlet weak var choreItem: UILabel!
+    @IBOutlet weak var choreItemLabel: UILabel!
 
     @IBOutlet weak var countLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        recordView.backgroundColor = .beigeFFF1E6
     }
     
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//        
-//    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
     
     func layoutCell(chores: [Chore]) {
         
         // chores 中的家事項目會是一樣的
-        choreItem.text = "總共完成 \(chores[0].item)"
+        choreItemLabel.text = "總共完成 \(chores[0].item)"
         
-        countLabel.text = "\(chores.count) 次"
+        countLabel.text = "\(chores.count)次"
         
         if let imageName = ChoreImages.imageNames[chores[0].item] {
             
@@ -44,5 +49,4 @@ class RecordsTableViewCell: UITableViewCell {
             choreImage.image = UIImage.asset(.CustomChore)
         }
     }
-    
 }
