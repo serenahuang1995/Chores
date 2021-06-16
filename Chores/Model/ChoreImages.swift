eb7415133871261e78ab7d440d25bff8857921fb
serenahuang1995 <small08261995@gmail.com>
2021-06-17 02:16:20 +0800

    Refactor ChoreImages file

--- Chores/Model/ChoreImages.swift
+++ Chores/Model/ChoreImages.swift
@@ -53,26 +53,27 @@ enum ChoreItem: String {
 
 struct ChoreImages {
     
-    static let imageNames: [String: String] = ["洗碗": "WashDishes",
-                                               "洗衣服": "WashClothes",
-                                               "晾衣服": "DryClothes",
-                                               "摺衣服": "FoldClothes",
-                                               "燙衣服": "IronClothes",
-                                               "倒垃圾": "ThrowTrash",
-                                               "刷廁所": "CleanBathroom",
-                                               "擦窗戶": "CleanWindow",
-                                               "修繕": "Repair",
-                                               "澆花": "WaterFlower",
-                                               "遛狗": "WalkDog",
-                                               "收納": "Storage",
-                                               "接送": "PickUp",
-                                               "帶小孩": "TakeCareKid",
-                                               "煮飯": "Cook",
-                                               "買菜": "Grocery",
-                                               "掃地": "BroomFloor",
-                                               "拖地": "MopFloor",
-                                               "吸地": "VacuumCleaner",
-                                               "其他": "CustomChore"
+    static let imageNames: [String: String] = [
+        "洗碗": "WashDishes",
+        "洗衣服": "WashClothes",
+        "晾衣服": "DryClothes",
+        "摺衣服": "FoldClothes",
+        "燙衣服": "IronClothes",
+        "倒垃圾": "ThrowTrash",
+        "刷廁所": "CleanBathroom",
+        "擦窗戶": "CleanWindow",
+        "修繕": "Repair",
+        "澆花": "WaterFlower",
+        "遛狗": "WalkDog",
+        "收納": "Storage",
+        "接送": "PickUp",
+        "帶小孩": "TakeCareKid",
+        "煮飯": "Cook",
+        "買菜": "Grocery",
+        "掃地": "BroomFloor",
+        "拖地": "MopFloor",
+        "吸地": "VacuumCleaner",
+        "其他": "CustomChore"
     ]
     
 }
