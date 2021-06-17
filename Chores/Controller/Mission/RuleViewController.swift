08aaada88e20493765743420ca7ede67b2c4439f
serenahuang1995 <small08261995@gmail.com>
2021-06-18 01:12:40 +0800

    rename RuleViewController variable name

--- Chores/Controller/Mission/RuleViewController.swift
+++ Chores/Controller/Mission/RuleViewController.swift
@@ -10,50 +10,26 @@ import MIBlurPopup
 
 class RuleViewController: UIViewController {
     
-    @IBOutlet weak var popView: CardView!
-    
-    //  let blackView = UIView(frame: UIScreen.main.bounds)
-    
+    @IBOutlet weak var cardView: CardView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //    showBlackView()
+
     }
     
-    @IBAction func backToAddChoresPage(_ sender: Any) {
+    @IBAction func navigateAddChoresPage(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
-        //    blackView.removeFromSuperview()
     }
-    
-    //  private func showBlackView() {
-    //
-    //    blackView.backgroundColor = .black
-    //    blackView.alpha = 0.7
-    //    presentingViewController?.view.addSubview(blackView)
-    //  }    
 }
 
 extension RuleViewController: MIBlurPopupDelegate {
     
-    var popupView: UIView {
-        
-        popView
-    }
+    var popupView: UIView { cardView }
     
-    var blurEffectStyle: UIBlurEffect.Style? {
-        
-        .dark
-    }
+    var blurEffectStyle: UIBlurEffect.Style? { .dark }
     
-    var initialScaleAmmount: CGFloat {
-        
-        0.0
-    }
+    var initialScaleAmmount: CGFloat { 0.0  }
     
-    var animationDuration: TimeInterval {
-        
-        0.2
-    }
-
+    var animationDuration: TimeInterval { 0.2 }
 }
