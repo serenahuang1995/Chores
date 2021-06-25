6de4cfc417d4ee2b6bc9b744dc5215edb4608e6c
serenahuang1995 <small08261995@gmail.com>
2021-06-25 23:22:21 +0800

    Tidy up Signin ViewController

--- Chores/Controller/Signin/SigninViewController.swift
+++ Chores/Controller/Signin/SigninViewController.swift
@@ -156,52 +156,6 @@ class SigninViewController: UIViewController {
         }
     }
     
-//    private func randomNonceString(length: Int = 32) -> String {
-//
-//        precondition(length > 0)
-//
-//        let charset: Array<Character> =
-//            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-//
-//        var result = ""
-//
-//        var remainingLength = length
-//
-//        while remainingLength > 0 {
-//
-//            let randoms: [UInt8] = (0 ..< 16).map { _ in
-//
-//                var random: UInt8 = 0
-//
-//                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-//
-//                if errorCode != errSecSuccess {
-//
-//                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-//                }
-//
-//                return random
-//            }
-//
-//            randoms.forEach { random in
-//
-//                if remainingLength == 0 {
-//
-//                    return
-//                }
-//
-//                if random < charset.count {
-//
-//                    result.append(charset[Int(random)])
-//
-//                    remainingLength -= 1
-//                }
-//            }
-//        }
-//
-//        return result
-//    }
-    
     @available(iOS 13, *)
     private func sha256(_ input: String) -> String {
         
@@ -270,8 +224,6 @@ extension SigninViewController: ASAuthorizationControllerDelegate {
                     guard let uid = Auth.auth().currentUser?.uid else { return }
                     
                     self.uid = uid
-//                    self.uid = "test7"
-//                    self.uid = "XC6b6Ys1VY1qLcBJ5M8z"
 
                     let userDefaults = UserDefaults()
                         
@@ -291,6 +243,7 @@ extension SigninViewController: ASAuthorizationControllerDelegate {
         print(error)
     }
 }
+
 extension SigninViewController: ASAuthorizationControllerPresentationContextProviding {
     
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
