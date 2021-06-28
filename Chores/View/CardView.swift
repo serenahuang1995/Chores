ff011e7c9a4d8bdac1253414126dddad2e3365de
serenahuang1995 <small08261995@gmail.com>
2021-06-28 17:56:17 +0800

    Tidy Card View

--- Chores/View/CardView.swift
+++ Chores/View/CardView.swift
@@ -12,14 +12,6 @@ class CardView: UIView {
     
     @IBInspectable var cornerRadius: CGFloat = 15
     
-    //  @IBInspectable var shadowOffsetWidth: CGFloat = 3
-    //  @IBInspectable var shadowOffsetHeight: CGFloat = 3
-    
-    // 控制陰影
-//      @IBInspectable var shadowColor: UIColor = UIColor.gray
-//      @IBInspectable var shadowOpacity: CGFloat = 0.5
-//    @IBInspectable var shadowRadius: CGFloat = 10
-    
     @IBInspectable var borderWidth: CGFloat = 1
     
     @IBInspectable var borderColor: UIColor = UIColor.clear
@@ -30,13 +22,10 @@ class CardView: UIView {
         layer.cornerRadius = cornerRadius
         
         // Shadow...........
-//        layer.shadowColor = shadowColor.cgColor
-//        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight)
         let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         
         layer.shadowPath = shadowPath.cgPath
-        // 控制陰影
-//        layer.shadowOpacity = Float(shadowOpacity)
+
         layer.masksToBounds = false
         
         // Border.............
@@ -44,5 +33,4 @@ class CardView: UIView {
         
         layer.borderColor = borderColor.cgColor
     }
-    
 }
